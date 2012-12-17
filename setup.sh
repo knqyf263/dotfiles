9f13ea422414cc7e40774d576aebb4d05ea93e49
knqyf263 <put.a.feud.pike011235@gmail.com>
2012-12-18 01:35:37 +0900

    modified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# tmuxのMac 用クリップボード設定
+ln -s $HOME/dotfiles/bin $HOME/bin
+
 DOT_FILES=( .zsh .zshrc .gitconfig .vimrc .vim .tmux.conf )
 
 for file in ${DOT_FILES[@]}
@@ -7,5 +10,3 @@ do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
-# tmuxのMac 用クリップボード設定
-ln -s $HOME/dotfiles/bin $HOME/bin
