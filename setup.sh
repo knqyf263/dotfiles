e62a6241ecf2f5df489e077bcc699fa440bd6b81
knqyf263 <put.a.feud.pike011235@gmail.com>
2012-12-25 19:06:05 +0900

    add neobundle settings to setup.sh

--- setup.sh
+++ setup.sh
@@ -10,3 +10,6 @@ do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+git clone https://github.com/Shougo/vimproc ~/.vim/bundle/vimproc
