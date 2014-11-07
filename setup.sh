56655e1747535f6c4669f04da6e9d56a8e126292
knqyf263 <put.a.feud.pike011235@gmail.com>
2014-11-07 15:44:19 +0900

    modified setup.sh for tmux-powerlinerc

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # tmuxのMac 用クリップボード設定
 ln -s $HOME/dotfiles/bin $HOME/bin
 
-DOT_FILES=( .zsh .zshrc* .gitconfig .dir_colors .vimrc .vim .tmux.conf )
+DOT_FILES=( .zsh .zshrc* .gitconfig .dir_colors .vimrc .vim .tmux.conf .tmux-powerlinerc )
 
 for file in ${DOT_FILES[@]}
 do
@@ -14,5 +14,10 @@ mkdir -p ~/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 git clone https://github.com/Shougo/vimproc ~/.vim/bundle/vimproc
 
-ln -s ~/dotfiles/tmux-powerline/segments/tmux-mem-cpu-load /usr/local/bin/
-
+# for tmux-mem-cpu-load
+git clone https://github.com/thewtex/tmux-mem-cpu-load.git
+cd ~/dotfiles/tmux-mem-cpu-load/
+cmake .
+make
+sudo make install
+rm -rf ~/dotfiles/tmux-mem-cpu-load/
