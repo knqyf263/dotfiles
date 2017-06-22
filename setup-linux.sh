d895ebda3860384388d764bebf0310f48d739080
knqyf263 <knqyf263@gmail.com>
2017-06-22 09:06:43 +0900

    Update

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .zsh .zshrc* .gitconfig .dir_colors .vimrc .vim .tmux.conf .tmux-powerlinerc .ideavimrc)
+DOT_FILES=( .zsh.d .zshrc* .gitconfig .dir_colors .vimrc .vim .tmux.conf .tmux-powerlinerc .ideavimrc)
 
 for file in ${DOT_FILES[@]}
 do
@@ -17,20 +17,13 @@ fc-cache -fv
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-mkdir -p ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-git clone https://github.com/Shougo/vimproc ~/.vim/bundle/vimproc
-
-# for tmux-mem-cpu-load
-git clone https://github.com/thewtex/tmux-mem-cpu-load.git
-cd ~/dotfiles/tmux-mem-cpu-load/
-cmake .
-make
-sudo make install
-rm -rf ~/dotfiles/tmux-mem-cpu-load/
-
-# Ruby関連
-# http://qiita.com/mogulla3/items/42a7f6c73fa4a90b1df3
-
-# for rsense
-source ./scripts/rsense.sh
+#mkdir -p ~/.vim/bundle
+#git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+#git clone https://github.com/Shougo/vimproc ~/.vim/bundle/vimproc
+#
+#
+## Ruby関連
+## http://qiita.com/mogulla3/items/42a7f6c73fa4a90b1df3
+#
+## for rsense
+#source ./scripts/rsense.sh
